8f9ac559f7177ef24afe29d70954efe82bc10ee7
snehil keshari <snehil585@gmail.com>
2023-03-08 13:42:55 -0500

    Create Promotions and test_create Promotions

--- service/routes.py
+++ service/routes.py
@@ -34,6 +34,46 @@ def index():
 ######################################################################
 # GET ALL PROMOTIONs
 ######################################################################
+
+@app.route("/promotions", methods=["POST"])
+def create_promotion():
+    """
+    Creates a Promotion
+    This endpoint will create a Promotion based the data in the body that is posted
+    """
+    
+    app.logger.info("Request to create a promotion")
+    check_content_type("application/json")
+    
+    promotion = Promotion()
+    promotion.deserialize(request.get_json())
+    promotion.create()
+    message = promotion.serialize()
+    
+
+    app.logger.info("Promotion with ID [%s] created.", promotion.id)
+    return jsonify(message), status.HTTP_201_CREATED
+
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {content_type}",
+    )
+
+
+
 @app.route("/promotions", methods=["GET"])
 def get_promotions():
     """
